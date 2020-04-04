ccf25e89516f8345040630ed6d6a071a63f5cafe
ajanthan <ajanthan.kt@gmail.com>
2020-04-04 13:50:59 +1100

    Intial Setup

---
+++ Contacts.Recorder/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Contacts.Recorder.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
