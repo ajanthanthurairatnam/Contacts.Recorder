73cbf4a8c2d1e749dade035d9f969699dfcd9b8f
ajanthan <ajanthan.kt@gmail.com>
2020-04-06 08:52:30 +1000

    Hide Secre

--- Contacts.Recorder/Services/ConfigurationService.cs
+++ Contacts.Recorder/Services/ConfigurationService.cs
@@ -15,10 +15,8 @@ namespace Contacts.Recorder.API.Services
         public ConfigurationService(IConfiguration Config)
         {
             _Config = Config;
-            Configuration.ConnectionString = @"mongodb://contact:rKkAMlY8pHlQ3ROcgIwuzNCJzW1iJEbteOkPTz0I6T0yXtPIdnISuLS60WIRhujod3C9pYkc3jMmh2K5qhzXBQ==@contact.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@contact@;retrywrites = false";
-
-
-            ///_Config.GetConnectionString("DBConnection");
+           
+            _Config.GetConnectionString("DBConnection");
         }
      
     }
